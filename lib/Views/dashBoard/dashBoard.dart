220df3d7a568d2bcf2e23fa65f81e9415cbe5b75
EwigLife <zain@ewiglife.com>
2022-08-03 23:30:21 +0500

    need to set colors

--- lib/Views/dashBoard/dashBoard.dart
+++ lib/Views/dashBoard/dashBoard.dart
@@ -1,16 +1,8 @@
 // ignore_for_file: file_names
 import 'package:flutter/material.dart';
 
-import 'package:jsoadminpanel/Services/responsiveService.dart';
-import 'package:jsoadminpanel/Views/category/createCategory.dart';
-
-import 'package:sidebarx/sidebarx.dart';
-
 class DashBoard extends StatelessWidget {
-  DashBoard({Key? key}) : super(key: key);
-
-  final _controller = SidebarXController(selectedIndex: 0, extended: true);
-  // final _key = Get.find<MenuController>().scaffoldKey;
+  const DashBoard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
